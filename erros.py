efa8a6eb429117b6ac77832263cba3a93d38420a
PensadorA <gustavo_marques35@hotmail.com>
2022-01-15 02:29:00 -0300

    Versão 1.0, operador de soma adicionado com sucesso.

---
+++ erros.py
@@ -0,0 +1,9 @@
+
+
+class Erros:
+    def error_1(self):  # Erro, entrada inválida do tamanho da matriz
+        print('Dado inválido, por favor digite a coluna e linha separado por X, sem espaços.')
+        self.recebe_dados()
+
+    def error_2(self): # Erro, não é possível somar essa matriz
+        print('As matrizes informadas não podem ser somadas')
--- matrizes.py
+++ matrizes.py
@@ -1,26 +1,29 @@
-
-class Creat_matriz:
+from erros import  *
+class Creat_matriz(Erros):
     def __init__(self):
         self.dados1 = []
         self.dados2 = []
+        self.tamanho = []
+        self.resultado =[]
         self.recebe_dados()
         self.tipo_operacao()
 
     def recebe_dados(self):
         self.cont_loop = 1
         while True:
-            if self.cont_loop ==3:
+            if self.cont_loop == 3:
                 break
             self.size_matrizes = str(input(f'Qual o tamanho da matriz {self.cont_loop}? Utilize X, ColXLin: '))
+            self.tamanho.append(self.size_matrizes)
             self.coluna, self.linha = self.fatia_string(self.size_matrizes)
             self.cont_linha = 1
             self.cont_coluna = self.coluna
             self.cont = 1
             for i in range(self.coluna*self.linha):
-                if self.cont_loop ==1:
-                    self.dados1 = int(input(f'Digite os dado da linha {self.cont_linha}: '))
+                if self.cont_loop == 1:
+                    self.dados1.append(int(input(f'Digite os dado da linha {self.cont_linha}: ')))
                 else:
-                    self.dados2 = int(input(f'Digite os dado da linha {self.cont_linha}: '))
+                    self.dados2.append(int(input(f'Digite os dado da linha {self.cont_linha}: ')))
                 if self.cont >= self.cont_coluna:
                     self.cont_linha += 1
                     self.cont_coluna += self.coluna
@@ -29,9 +32,12 @@ class Creat_matriz:
 
 
     def fatia_string(self, size):
-        self.fatiado = size.split('X')
-        self.size_matrize = list(map(int, self.fatiado))
-        return self.size_matrize[0], self.size_matrize[1]
+        try:
+            self.fatiado = size.split('X')
+            self.size_matrize = list(map(int, self.fatiado))
+            return self.size_matrize[0], self.size_matrize[1]
+        except:
+            self.error_1()
 
     def tipo_operacao(self):
         self.tipo = str(input('Qual será o tipo de operação? (+, -, *, /'))
@@ -39,7 +45,16 @@ class Creat_matriz:
             self.operacao_soma()
 
     def operacao_soma(self):
-        pass
+        if self.tamanho[0] == self.tamanho[1]:
+            print(self.dados1)
+            for i in range(len(self.dados1)):
+                self.resultado.append(self.dados1[i] + self.dados2[i])
+            print(f'Sua matriz resultante  da soma é: {self.resultado}')
+        else:
+            self.error_2()
+
+
+
 
 
 
